0f37cd81c6f2da8d99ba1fad3d0a7d36aef9ad70
Dario Bonotto <dbonotto@atlassian.com>
2018-08-09 09:17:56 +0000

    backup.sh Working as per 09/08/2018

--- backup.sh
+++ backup.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
-# NOT - This script doesn't currently work with the current version of Jira Cloud as of Dec 10th, 2017.
-# Try the .ps1 script instead (which has been already updated).
+USERNAME=
+PASSWORD=
+INSTANCE=
+LOCATION="/path/to/download/folder"
 
-USERNAME=youruser
-PASSWORD=yourpassword
-INSTANCE=example.atlassian.net
-LOCATION=/where/to/store/the/file
+### Checks for progress 3000 times every 20 seconds ###
+PROGRESS_CHECKS=3000
+SLEEP_SECONDS=20
 
 # Set this to your Atlassian instance's timezone.
 # See this for a list of possible values:
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-TIMEZONE=America/Los_Angeles
+TIMEZONE=Europe/Amsterdam
  
+##----START-----###
+echo "starting the script"
+
 # Grabs cookies and generates the backup on the UI. 
 TODAY=$(TZ=$TIMEZONE date +%Y%m%d)
 COOKIE_FILE_LOCATION=jiracookie
 curl --silent --cookie-jar $COOKIE_FILE_LOCATION -X POST "https://${INSTANCE}/rest/auth/1/session" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" -H 'Content-Type: application/json' --output /dev/null
-#The $BKPMSG variable will print the error message, you can use it if you're planning on sending an email
-BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION --header "X-Atlassian-Token: no-check" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json"  -X POST https://${INSTANCE}/rest/obm/1.0/runbackup -d '{"cbAttachments":"true" }' )
+
+## The $BKPMSG variable will print the error message, you can use it if you're planning on sending an email
+BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION -H "Accept: application/json" -H "Content-Type: application/json" https://${INSTANCE}/rest/backup/1/export/runbackup --data-binary '{"cbAttachments":"true", "exportToCloud":"true"}' )
+
+
+##ADDED##
+echo "message: $BKPMSG"
+
  
 #Checks if the backup procedure has failed
-if [ "$(echo "$BKPMSG" | grep -ic backup)" -ne 0 ]; then
+if [ "$(echo "$BKPMSG" | grep -ic error)" -ne 0 ]; then
 rm $COOKIE_FILE_LOCATION
+echo "FAILED, IT RETURNED: $BKPMSG"
 exit
 fi
 
-#Checks if the backup exists every 10 seconds, 20 times. If you have a bigger instance with a larger backup file you'll probably want to increase that.
-for (( c=1; c<=20; c++ ))
+TASK_ID=$(curl -s --cookie $COOKIE_FILE_LOCATION -H "Accept: application/json" -H "Content-Type: application/json" https://${INSTANCE}/rest/backup/1/export/lastTaskId)
+
+#Checks if the backup exists every 10 seconds, 2000 times. If you have a bigger instance with a larger backup file you'll probably want to increase that.
+for (( c=1; c<=2000; c++ ))
 do
-PROGRESS_JSON=$(curl -s --cookie $COOKIE_FILE_LOCATION https://${INSTANCE}/rest/obm/1.0/getprogress.json)
-FILE_NAME=$(echo "$PROGRESS_JSON" | sed -n 's/.*"fileName"[ ]*:[ ]*"\([^"]*\).*/\1/p')
+PROGRESS_JSON=$(curl -s --cookie $COOKIE_FILE_LOCATION https://${INSTANCE}/rest/backup/1/export/getProgress?taskId=${TASK_ID})
+FILE_NAME=$(echo "$PROGRESS_JSON" | sed -n 's/.*"result"[ ]*:[ ]*"\([^"]*\).*/\1/p')
+
+##ADDED##
+echo "$PROGRESS_JSON"
 
 if [[ $PROGRESS_JSON == *"error"* ]]; then
 break
@@ -42,21 +58,19 @@ fi
 sleep 10
 done
 
-#If after 20 attempts it still fails it ends the script.
+#If after 2000 attempts it still fails it ends the script.
 if [ -z "$FILE_NAME" ];
 then
 rm $COOKIE_FILE_LOCATION
 exit
 else
 
-#If it's confirmed that the backup exists the file get's copied to the $LOCATION directory.
-if [[ $FILE_NAME == *"ondemandbackupmanager/download"* ]]; then
-#Download the new way, starting Nov 2016
-wget --load-cookies=$COOKIE_FILE_LOCATION -t 0 --retry-connrefused "https://${INSTANCE}/$FILE_NAME" -O "$LOCATION/JIRA-backup-${TODAY}.zip" >/dev/null 2>/dev/null
-else
-#Deprecated download from WEBDAV
-echo "Attempted to download from WEBDAV directory, which is no longer supported"
-fi
+## PRINT THE FILE TO DOWNLOAD ##
+echo "File to download: https://${INSTANCE}/plugins/servlet/${FILE_NAME}"
+
+curl -s -L --cookie $COOKIE_FILE_LOCATION "https://${INSTANCE}/plugins/servlet/${FILE_NAME}" -o "$LOCATION/JIRA-backup-${TODAY}.zip"
+
+
 
 fi
 rm $COOKIE_FILE_LOCATION
